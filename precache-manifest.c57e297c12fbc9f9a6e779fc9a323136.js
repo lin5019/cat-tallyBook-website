22198c6eb803a9aa9ecf229ac6a69c44f1793d58
lin <13889305019@139.com>
2021-01-08 01:23:03 +0800

    deploy

---
+++ precache-manifest.c57e297c12fbc9f9a6e779fc9a323136.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f69ffd3d5a56c5531e47",
+    "url": "/cat-tallyBook-website/css/app.b33f0512.css"
+  },
+  {
+    "revision": "3fddb9d4ca252ce2a06d8e8c4ecf5209",
+    "url": "/cat-tallyBook-website/img/二维码.3fddb9d4.png"
+  },
+  {
+    "revision": "520f491438c7654fd925cc5b1173747d",
+    "url": "/cat-tallyBook-website/index.html"
+  },
+  {
+    "revision": "f69ffd3d5a56c5531e47",
+    "url": "/cat-tallyBook-website/js/app.2454131b.js"
+  },
+  {
+    "revision": "5f81347bf283b92e9107",
+    "url": "/cat-tallyBook-website/js/chunk-vendors.ad3f9ca7.js"
+  },
+  {
+    "revision": "8ac8d6b98a25a086ea3cd8a98c0a0096",
+    "url": "/cat-tallyBook-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cat-tallyBook-website/robots.txt"
+  }
+]);
